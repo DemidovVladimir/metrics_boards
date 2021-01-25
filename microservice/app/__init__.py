19bb83b50ac9a1d8b5b00f6a5dd703a4dd0c7c70
DemidovVladimir <uncojet@gmail.com>
2021-01-25 15:26:10 +0100

    use flask

--- microservice/app/__init__.py
+++ microservice/app/__init__.py
@@ -3,7 +3,7 @@ from prometheus_flask_exporter import PrometheusMetrics
 from flask import Flask
 app = Flask(__name__)
 
-metrics = PrometheusMetrics(app)
+metrics = PrometheusMetrics(app, group_by='endpoint')
 metrics.info("app_info", "App Info, this can be anything you want", version="1.0.0")
 
 from app import views
--- microservice/app/views.py
+++ microservice/app/views.py
@@ -1,7 +1,12 @@
 from flask import render_template
-from app import app
+from app import app, metrics
+from prometheus_flask_exporter import PrometheusMetrics
 
 @app.route('/')
+@metrics.counter(
+    'cnt_collection', 'Number of invocations per collection', labels={
+        'status': lambda resp: resp.status_code
+    })
 def home():
    return "BMW rules!"
 
